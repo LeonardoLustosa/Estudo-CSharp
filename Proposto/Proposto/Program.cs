f19f5688007478c2c656aaf1f25f136b5971aa4d
Leonardo <leonardo.a.lustosa2@gmail.com>
2025-02-14 19:11:54 -0300

    Add file via upload

---
+++ Proposto/Proposto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            string nome, sobrenome;
+            int Quartos, idade;
+            double produto, altura;
+            
+
+            Console.WriteLine("Entre com seu nome completo: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa: ");
+            Quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dê um preço pra vc: ");
+            produto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com sobrenome, idade e altura");
+            string[] vet = Console.ReadLine().Split(' ');
+
+            sobrenome = vet[0];
+            idade = int.Parse(vet[1]);
+            altura = double.Parse(vet[2], CI);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Sua lista de corno: ");
+            Console.WriteLine(nome);
+            Console.WriteLine(Quartos);
+            Console.WriteLine(produto);
+            Console.WriteLine(sobrenome + " " + idade + " " + altura.ToString("F2", CI) );
+
+
+
+        }
+    }
+}
---
+++ Prova 1/Prova 1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+
+internal class Program {
+    private static void Main(string[] args) {
+
+        CultureInfo CI = CultureInfo.InvariantCulture;
+        double peça1, peça2, Q1, Q2, v1, v2, soma;
+        int produtos;
+
+        Console.WriteLine("Quantos produtos: ");
+        produtos = int.Parse(Console.ReadLine());
+
+        if (produtos > 1 && produtos <= 2) {
+            Console.WriteLine("Digite o Codigo das peças: ");
+            peça1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Quantidade: ");
+            Q1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite valor: ");
+            v1 = double.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine("Digite o Codigo das peças: ");
+            peça2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Quantidade: ");
+            Q2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite valor: ");
+            v2 = double.Parse(Console.ReadLine(), CI);
+
+            soma = v1 + v2;
+            Console.WriteLine("Valor total a pagar = " + soma.ToString("F2", CI));
+
+        }
+        else {
+            Console.WriteLine("Digite o Codigo das peças: ");
+            peça1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite valor: ");
+            v1 = double.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine("Valor total a pagar " + v1.ToString("F2", CI));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
