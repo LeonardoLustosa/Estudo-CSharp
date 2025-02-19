df0c19d395539a063d47c1a3e1460ee826d05a15
Leonardo <leonardo.a.lustosa2@gmail.com>
2025-02-19 18:56:52 -0300

    Add files via upload

---
+++ POO5/POO5/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Program {
+    internal class Produto {
+
+        public CultureInfo CI = CultureInfo.InvariantCulture;
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+
+            return (Preco * Quantidade);
+        }
+        public void AdicionarProdutos(int qtd) {
+
+            Quantidade = Quantidade + qtd;
+
+        }
+        public void RetirarProdutos(int qtd2) {
+
+            Quantidade = Quantidade - qtd2;
+
+        }
+        public override string ToString() {
+            return Nome + ", $" + Preco.ToString("F2", CI)
+                + ", " + Quantidade + " Unidades, " + "Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CI);
+
+
+        }
+
+
+    }
+}
+
---
+++ POO5/POO5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Program;
+
+namespace Projeto {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto();
+
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), p.CI);
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite um numero de produto a ser adicionado ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite um numero de produto a ser retirado do estoque: ");
+            int qtd2 = int.Parse(Console.ReadLine());
+            p.RetirarProdutos(qtd2);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);    
+
+        }
+    }
+}
